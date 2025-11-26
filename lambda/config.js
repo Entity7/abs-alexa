3a584627f9f74d7dbe9e8c4ebee4fa424732e2b2
Entity7 <82467899+Entity7@users.noreply.github.com>
2025-11-26 07:57:08 -0800

    Update API key and server URL in config.js

--- lambda/config.js
+++ lambda/config.js
@@ -1,7 +1,7 @@
 // config.js
 module.exports = {
-    ABS_API_KEY: 'xxxxxx',
-    SERVER_URL: 'https://abs.domain.tld',
+    ABS_API_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXlJZCI6IjQ2ZDIyODk1LTM5Y2QtNDY4YS04ZjQzLWNmMDAxZGY2NTcxMSIsIm5hbWUiOiJBbGV4YSBJbnRlZ3JhdGlvbiIsInR5cGUiOiJhcGkiLCJpYXQiOjE3NjQxNzI1MjZ9.dknx9_0Kls0fEbhuhzSL-EVh9aOCh1lk2H8ZG1nNFFM',
+    SERVER_URL: 'http://entitys.ddns.net:13378',
     USER_AGENT: 'AlexaSkill'
     // OTHER OPTIONAL HEADERS
     // CFAccessClientId: 'xxxxxxxxx.access',
